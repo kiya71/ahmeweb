d87a38a540b79c2cd6441f9918b5691f62004656
kiya71 <kiyaoro1@gmail.com>
2024-08-22 15:37:08 +0300

    Add files via upload

---
+++ Own Web/js/animation.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing",
+    {
+        strings:["","Web Designer","Front-End Developer","Backend-End Developer"],
+        typeSpeed:100,
+        BackSpeed:60,
+        loop:true
+    }
+)
